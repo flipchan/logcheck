128d19b62567e4ba455a822c7c2e5d75f82fd301
flipchan <flipchan@users.noreply.github.com>
2019-10-29 01:03:37 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ extern crate lmdb_zero as lmdb;
 
 use std::io::{BufReader,BufRead};
 use std::fs::File;
+use std::env;
 
+//plan
+// call it like ./logcheck /file/path/to/access/log
+//regex out the ips and unique sort and count it, stream parse the 
 
+fn grepip(lineoffile: &str) str{
+  return "ip here";
+}
 
 fn main() {
-  println!("starting!")
-  let openme = "file here";
+  println!("logcheck by flipchan")
+     let args: Vec<String> = env::args().collect();
+    let openme = &args[1];
+ // let openme = "file here";
   let file = File::open(openme).unwrap();
   println!("file is open!");
- for line in BufReader::new(file).lines() {
+ for line in BufReader::new(file).lines() { //stream parse the file
     //do things
                                             }
 }
