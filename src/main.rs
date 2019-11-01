0c00e46193453016f8de341d94de0327531188fd
flipchan <flipchan@users.noreply.github.com>
2019-11-01 02:58:54 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,24 @@ extern crate lmdb_zero as lmdb;
 use std::io::{BufReader,BufRead};
 use std::fs::File;
 use std::env;
+use regex::Regex;
+use std::collections::HashSet; // use it like python's set()
+
 
 //plan
 // call it like ./logcheck /file/path/to/access/log
 //regex out the ips and unique sort and count it, stream parse the 
 
 fn grepip(lineoffile: &str) str{
+  let ipv4find = Regex::new("").unwrap(); //ipv4 regex
+  let findings = ipv4find.find(&lineoffile).unwrap();
+  println!("i have found", findings);
   return "ip here";
 }
 
 fn main() {
   println!("logcheck by flipchan")
+  let mut ipcounter = HashSet::new();
      let args: Vec<String> = env::args().collect();
     let openme = &args[1];
  // let openme = "file here";
@@ -23,5 +30,6 @@ fn main() {
   println!("file is open!");
  for line in BufReader::new(file).lines() { //stream parse the file
     //do things
+   if grepip(line) != "nope"{ //append to global uniq list }
                                             }
 }
