8d7f50d70317369fe2b014df49ce6d26cb60524b
root <root@burk0>
2020-04-03 11:08:42 +0200

    new things

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /var/log/hiawatha/blogaccess.log|uniq| wc -l does not scale
 extern crate regex;
+extern crate chrono;
+use chrono::{DateTime, Utc};
 
 use std::io::{BufReader,BufRead};
 use std::fs::File;
@@ -11,6 +13,33 @@ use std::process::exit;
 //plan
 // call it like ./logcheck /file/path/to/access/log
 //regex out the ips and unique sort and count it, stream parse the 
+
+fn todaysstuff(filen: String) { //take path as string
+// different logs have different formats | openbsd httpd has domain.com ipaddress - - [03/Apr/2020:11:10:00 +0300] "GET / 
+//											this i need to find
+// ideally it should auto detect the log format based regex matches
+	let now: DateTime<Utc> = Utc::now();
+	let dateprint = now.format("%Y-%m-%d");
+	let sumtag = now.format("%a %b %e %Y");
+	let this = now.format("%d/%b/%Y");
+	let this = format!("{}", this);
+	let mut counter = 0;
+	println!("Checking visitors for today: {}", sumtag);
+
+	  let file = File::open(filen).unwrap();
+	 for line in BufReader::new(file).lines() { //stream parse the file
+	    let linje = line.as_ref().unwrap();
+	    let mut parts = linje.to_string();
+	     if parts.contains(&this) { // if we find todays date in the log file
+		counter += 1;
+//		amount.insert(caps.get(0).unwrap().as_str().to_string());   // insert the 
+	    }
+			    }
+
+	println!("Amount of visitors today: {}", counter);
+
+}
+
 
 fn main() {
   println!("logcheck by flipchan");
